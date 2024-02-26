681881c9fd7a7611f24aea21b92a401b06aef912
gaurav9359 <114300073+gaurav9359@users.noreply.github.com>
2024-02-26 17:39:04 +0530

    add task feature added

--- src/app/Components/todo-list/todo-list.component.ts
+++ src/app/Components/todo-list/todo-list.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { todoListClass } from '../../Shared/todoListClass';
+import { NgIf} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [NgIf,FormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
-export class TodoListComponent {
-    list= [
-      new todoListClass("cricket", true),
-      new todoListClass("badminton", true),
-      new todoListClass("khokho", false)
 
+export class TodoListComponent {
+    list : todoListClass[]= [
+      // new todoListClass("cricket", true),
+      // new todoListClass("badminton", true),
+      // new todoListClass("khokho", false)
     ]
+
+    newTask=''
+
+    handleNewTask(){
+      this.list.push(new todoListClass(this.newTask,false))
+      this.newTask=''
+    }
+
+    handleChecked(e: todoListClass){
+      e.isCompleted= !e.isCompleted
+    }
 }
